99d30dbcb148a2de437ed7bb626faaa13a3a3fc8
Salmane Sarghini <ss.sarghini@gmail.com>
2026-07-17 12:34:24 +0100

    fixed it to work on github this time

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ btn.addEventListener("click", () => {
     const password = document.getElementById("password").value;
 
     if (email === "Salmane" && password === "123") {
-        window.location.href = "Ticket/ticket.html";
+        window.location.href = "https://voidl0op.github.io/Ticketing/Ticket/ticket.html";
     } else {
         btn.classList.add("btn-error");
         btn.textContent = "Informations incorrectes";
